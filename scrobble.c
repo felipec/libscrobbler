665d014fa4f63015505dc620185123d9d0c7d31b
Felipe Contreras <felipe.contreras@gmail.com>
2010-01-01 15:55:19 +0200

    Add track stuff
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- scrobble.c
+++ scrobble.c
@@ -15,3 +15,19 @@ sr_session_free(sr_session_t *s)
 {
 	free(s);
 }
+
+sr_track_t *
+sr_track_new(void)
+{
+	sr_track_t *t;
+	t = calloc(1, sizeof(*t));
+	return t;
+}
+
+void
+sr_track_free(sr_track_t *t)
+{
+	free(t->artist);
+	free(t->title);
+	free(t);
+}
--- scrobble.h
+++ scrobble.h
@@ -1,6 +1,13 @@
 #ifndef SCROBBLE_H
 #define SCROBBLE_H
 
+typedef struct sr_track sr_track_t;
+
+struct sr_track {
+	char *artist;
+	char *title;
+};
+
 typedef struct sr_session sr_session_t;
 
 struct sr_session {
@@ -8,5 +15,8 @@ struct sr_session {
 
 sr_session_t *sr_session_new(void);
 void sr_session_free(sr_session_t *s);
+
+sr_track_t *sr_track_new(void);
+void sr_track_free(sr_track_t *t);
 
 #endif /* SCROBBLE_H */
