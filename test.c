2ea066b6c1216c46e9cd1cc0d821d12b1c0d3e18
Felipe Contreras <felipe.contreras@gmail.com>
2010-01-02 16:00:19 +0200

    test: reorganize cred loading
    Move the keyfile to main, will be useful for multi-scrobbling.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- test.c
+++ test.c
@@ -9,20 +9,11 @@ static GMainLoop *main_loop;
 
 static gboolean
 load_cred(sr_session_t *s,
+	  GKeyFile *keyfile,
 	  const char *id)
 {
-	gchar *file;
-	GKeyFile *keyfile;
 	gchar *username = NULL, *password = NULL;
 	gboolean ok;
-
-	keyfile = g_key_file_new();
-	file = g_build_filename(g_get_user_config_dir(),
-				"scrobbler", NULL);
-
-	ok = g_key_file_load_from_file(keyfile, file, G_KEY_FILE_NONE, NULL);
-	if (!ok)
-		goto leave;
 
 	username = g_key_file_get_string(keyfile, id, "username", NULL);
 	password = g_key_file_get_string(keyfile, id, "password", NULL);
@@ -36,7 +27,6 @@ load_cred(sr_session_t *s,
 leave:
 	g_free(username);
 	g_free(password);
-	g_key_file_free(keyfile);
 
 	return ok;
 }
@@ -57,15 +47,27 @@ static gboolean timeout(void *data)
 
 int main(void)
 {
-	sr_session_t *s;
+	sr_session_t *s = NULL;
+	GKeyFile *keyfile;
+	gchar *file;
+	gboolean ok;
 
 	g_type_init();
 	if (!g_thread_supported())
 		g_thread_init(NULL);
 
+	keyfile = g_key_file_new();
+
+	file = g_build_filename(g_get_user_config_dir(),
+				"scrobbler", NULL);
+
+	ok = g_key_file_load_from_file(keyfile, file, G_KEY_FILE_NONE, NULL);
+	if (!ok)
+		goto leave;
+
 	s = sr_session_new(SR_LASTFM_URL, "tst", "1.0");
 	s->error_cb = error_cb;
-	load_cred(s, "lastfm");
+	load_cred(s, keyfile, "lastfm");
 	sr_session_load_list(s, "list");
 	sr_session_test(s);
 	sr_session_store_list(s, "foo");
@@ -74,6 +76,10 @@ int main(void)
 
 	main_loop = g_main_loop_new(NULL, FALSE);
 	g_main_loop_run(main_loop);
+
+leave:
 	sr_session_free(s);
+	g_key_file_free(keyfile);
+
 	return 0;
 }
